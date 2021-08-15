8f8fec14c24932e14462feac312689d10a816fda
Guilhem Mathieux <guilhem.mathieux@u-psud.fr>
2021-08-15 14:15:36 +0200

    test for similar

---
+++ tests/test_Utilitaries.py
@@ -0,0 +1,33 @@
+# -*-coding:utf-8 -*
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # patch for imports
+os.chdir(os.path.dirname(__file__)) # places in corect dir
+
+import time 
+
+from mep import Utilitaries
+
+def main() :
+    lst =["Bing Crosby",
+        "Elton John",
+        "Mungo Jerry",
+        "Bill Haley & His Comets",
+        "Domenico Modugno",
+        "Whitney Houston",
+        "Elvis Presley",
+        "USA for Africa",
+        "The Ink Spots",
+        "Céline Dion",
+        "The Beatles",
+        "John Travolta et Olivia Newton",
+        "Bryan Adams"]
+    res = ""
+    for a in lst :
+        for b in lst :
+            if  Utilitaries.similar(a,b) :
+                res+=(f'{a} = {b} ')
+    assert(res=="Bing Crosby = Bing Crosby Elton John = Elton John Mungo Jerry = Mungo Jerry Bill Haley & His Comets = Bill Haley & His Comets Domenico Modugno = Domenico Modugno Whitney Houston = Whitney Houston Elvis Presley = Elvis Presley USA for Africa = USA for Africa The Ink Spots = The Ink Spots Céline Dion = Céline Dion The Beatles = The Beatles John Travolta et Olivia Newton = John Travolta et Olivia Newton Bryan Adams = Bryan Adams ")
+    print("similar works")
+    
+if __name__ == '__main__':
+    main()
